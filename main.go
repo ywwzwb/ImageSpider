e9141d0bd11be07b5f7459606c6d296e8f86b3b6
zengwenbin <zengwenbin.zwb@antgroup.com>
2026-01-30 14:15:04 +0800

    feat: 增加图片测试代码

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "ywwzwb/imagespider/app"
 
+// import (
+// 	"log/slog"
+// 	"os"
+// 	"ywwzwb/imagespider/plugins"
+// )
+
 func main() {
 	app.GetApplication().Run()
+	// h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	// 	Level: slog.LevelDebug,
+	// })
+	// l := slog.New(h)
+	// slog.SetDefault(l)
+	// f := plugins.FileIntegrityChecker{}
+	// f.RunTest()
 }
